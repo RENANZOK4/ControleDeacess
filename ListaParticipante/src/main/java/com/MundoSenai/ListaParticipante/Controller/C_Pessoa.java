227816bb1b7b67bd8fadbba1cb550f39bd17e45d
RENANZOK4 <117952546+RENANZOK4@users.noreply.github.com>
2023-07-28 19:22:48 -0300

    Add files via upload

---
+++ ListaParticipante/src/main/java/com/MundoSenai/ListaParticipante/Controller/C_Pessoa.java
@@ -0,0 +1,10 @@
+package com.MundoSenai.ListaParticipante.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class C_Pessoa {
+    public String landPage(){
+        return "login/Login";
+    }
+}
---
+++ ListaParticipante/src/test/java/com/MundoSenai/ListaParticipante/ListaParticipanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MundoSenai.ListaParticipante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaParticipanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
